0fca8e0cace3a3e0ce9af0847fc68c7aa13e6ae7
David Rosillo <drr538@gmail.com>
2015-08-31 19:30:53 -0500

    Added relations to models

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,4 @@
 class Portfolio < ActiveRecord::Base
+	has_many :portfolio_categories
+	has_many :categories, through: :portfolio_categories
 end
--- app/models/portfolio_category.rb
+++ app/models/portfolio_category.rb
@@ -1,2 +1,4 @@
 class PortfolioCategory < ActiveRecord::Base
+	has_many :portfolio_categories
+	has_many :portfolios, through: :portfolio_categories
 end

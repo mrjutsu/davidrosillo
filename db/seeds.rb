e800546ec8bcddbda87b15176cf2c45a713223ba
David Rosillo <drr538@gmail.com>
2015-09-01 20:14:39 -0500

    Create seeds (temporary admin)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+#CREATE CATEGORIES
+
+categories = [
+	{ name: "Life" },
+	{ name: "How To" },
+	{ name: "Games" },
+]
+
+Category.create( categories )
+
+#CREATE PORTFOLIO CATEGORIES
+
+p_cats = [
+	{ name: "Gem" },
+	{ name: "Javascript" },
+	{ name: "Ruby" },
+	{ name: "HTML" },
+	{ name: "CSS" },
+	{ name: "Android" },
+	{ name: "iOS" },
+]
+
+PortfolioCategory.create( p_cats )

347f5ce22335c3c02b1b86652676793eed9467d2
David Rosillo <drr538@gmail.com>
2015-09-01 20:04:20 -0500

    Finish activeadmin resources initial config

--- app/admin/subcategory.rb
+++ app/admin/subcategory.rb
@@ -1,17 +1,32 @@
 ActiveAdmin.register Subcategory do
+	permit_params :name, :category_id
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+	index title: "Subcategories" do
+		selectable_column
+		id_column
+		column :name
+		column :category
+		column :created_at
+	end
 
+	filter name:
+	filter :category
+
+	form html: { enctype: "multipart/form-data" } do |f|
+		f.inputs "Post" do
+			f.input :name
+			f.input :category, as: :select, collection: Category.all.map { |x| [x.name,x.id] }
+		end
+		f.actions
+	end
+
+	show title: "Subcategory" do
+		attributes_table do
+			row :id
+			row :created_at
+			row :name
+			row :category
+		end
+	end
 
 end

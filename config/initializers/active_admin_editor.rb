441ddaaa8545c4bed9cc0e30c5a6843af6f789ac
David Rosillo <drr538@gmail.com>
2015-09-04 19:20:05 -0500

    Comment unused things

--- config/initializers/active_admin_editor.rb
+++ config/initializers/active_admin_editor.rb
@@ -1,6 +1,6 @@
 ActiveAdmin::Editor.configure do |config|
-  config.s3_bucket = ENV["s3_bucket"]
-  config.aws_access_key_id = ENV["aws_key_id"]
-  config.aws_access_secret = ENV["aws_secret_key"]
-  # config.storage_dir = 'uploads'
+  # config.s3_bucket = ENV["s3_bucket"]
+  # config.aws_access_key_id = ENV["aws_key_id"]
+  # config.aws_access_secret = ENV["aws_secret_key"]
+  # config.storage_dir = '/'
 end

f63c676a071ca586941e4d77ca13d93445942fad
David Rosillo <drr538@gmail.com>
2015-09-07 12:09:08 -0500

    Add Mandrill config for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+    :authentication => 'login' # Mandrill supports 'plain' or 'login'
+  }
 end

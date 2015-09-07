58cade8e04c2bdc3034c0832bbac3c26859c2eb6
David Rosillo <drr538@gmail.com>
2015-09-07 12:05:49 -0500

    Create new contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,16 @@
 class ContactsController < InheritedResources::Base
 
 	def new
+		@contact = Contact.new
 	end
 
 	def create
+		@contact = Contact.new(contact_params)
+		if @contact.save
+			redirect_to root_path
+		else
+			redirect_to :back
+		end
 	end
 
   private

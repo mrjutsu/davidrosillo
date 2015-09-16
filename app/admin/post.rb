d7fd5f228d59334fb72efcf3af0d23fbe61270cf
David Rosillo <drr538@gmail.com>
2015-09-16 12:10:09 -0500

    Started styling post show, removed html_editor from post and made a test to support gists

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -19,7 +19,7 @@ ActiveAdmin.register Post do
 		f.inputs @post do
 			f.input :name
 			f.input :category, as: :select, collection: Category.all.map { |x| [x.name,x.id] }
-			f.input :body, as: :html_editor
+			f.input :body#, as: :html_editor
 		end
 		f.actions
 	end

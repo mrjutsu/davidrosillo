01a317b5235106b95630dbc4842a5ac3f25b5ab6
David Rosillo <drr538@gmail.com>
2015-09-16 14:41:30 -0500

    Add Disqus support for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  belongs_to :category
+	belongs_to :category
 
-  scope :by_category, -> (category_id) { where(category_id: category_id).order(created_at: :desc) }
-  scope :recent, -> { last(5) }
+	scope :by_category, -> (category_id) { where(category_id: category_id).order(created_at: :desc) }
+	scope :recent, -> { last(5) }
 end

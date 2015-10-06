2b1cce2a010e4438746ee446efa2d6bbbfc06468
David Rosillo <drr538@gmail.com>
2015-10-05 19:12:03 -0500

    Fix styles for mobile devices

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,7 +4,7 @@ class Portfolio < ActiveRecord::Base
 
 	accepts_nested_attributes_for :portfolio_portfolio_categories, :allow_destroy => true
 
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumbnail => "360", :ipadthumbnail => "820", :ipadshow => "1024", :phonethumb => "600", :phoneshow => "736" }, :default_url => "/images/:style/missing.png", :storage => :s3, :bucket => 'davidrosillo', :url => ':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename', :s3_protocol => 'https'
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumbnail => "360", :ipadthumbnail => "820", :ipadshow => "1024", :phonethumb => "800", :phoneshow => "736" }, :default_url => "/images/:style/missing.png", :storage => :s3, :bucket => 'davidrosillo', :url => ':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename', :s3_protocol => 'https'
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   	has_attached_file :thumbnail, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/missing.png"
